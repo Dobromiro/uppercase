baa8b3a3ccd0a7389d515cf445e461bd0d8fc082
mateusz.wernik <mateusz.wernik@HEP-N354>
2024-05-08 12:47:46 +0200

    Dodaj pliki projektów.

---
+++ uppercase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppercase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sciezka = Directory.GetCurrentDirectory();
+
+            try
+            {
+                string[] pliki = Directory.GetFiles(sciezka);
+
+                foreach (string plik in pliki)
+                {
+                    string nazwaPliku = Path.GetFileName(plik);
+                    string nowaNazwa = nazwaPliku.ToUpper();
+                    string nowaSciezka = Path.Combine(Path.GetDirectoryName(plik), nowaNazwa);
+                    File.Move(plik, nowaSciezka);
+                    Console.WriteLine($"Zmieniono nazwę pliku: {plik} na {nowaNazwa}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
